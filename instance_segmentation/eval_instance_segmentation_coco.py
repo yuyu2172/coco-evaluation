476a668167ec274515162c4a06bf9d9c7c7f9ede
Yusuke Niitani <yuyuniitani@gmail.com>
2018-06-06 19:31:16 +0900

    change names

--- instance_segmentation/eval_instance_segmentation_coco.py
+++ instance_segmentation/eval_instance_segmentation_coco.py
@@ -42,9 +42,9 @@ def eval_instance_segmentation_coco(sizes, pred_bboxes, pred_masks,
     gt_areas = (iter(gt_areas) if gt_areas is not None
                 else itertools.repeat(None))
 
-    images = list()
-    pred_anns = list()
-    gt_anns = list()
+    ids = list()
+    pred_annos = list()
+    gt_annos = list()
     unique_labels = dict()
     for i, (size, pred_bbox, pred_mask, pred_label, pred_score,
             gt_bbox, gt_mask, gt_label, gt_crowded, gt_area) in enumerate(
@@ -60,29 +60,29 @@ def eval_instance_segmentation_coco(sizes, pred_bboxes, pred_masks,
 
         pred_whole_mask = mask2whole_mask(pred_mask, pred_bbox, size)
         gt_whole_mask = mask2whole_mask(gt_mask, gt_bbox, size)
-        for pred_whole_m, pred_lbl, pred_sc in zip(
+        for pred_whole_m, pred_lb, pred_sc in zip(
                 pred_whole_mask, pred_label, pred_score):
-            pred_anns.append(
-                _create_ann(pred_whole_m, pred_lbl, pred_sc,
-                            img_id=img_id, ann_id=len(pred_anns) + 1,
-                            crw=0))
-            unique_labels[pred_lbl] = True
+            pred_annos.append(
+                _create_anno(pred_whole_m, pred_lb, pred_sc,
+                             img_id=img_id, anno_id=len(pred_annos) + 1,
+                             crw=0))
+            unique_labels[pred_lb] = True
 
-        for gt_whole_m, gt_lbl, gt_crw, gt_ar in zip(
+        for gt_whole_m, gt_lb, gt_crw, gt_ar in zip(
                 gt_whole_mask, gt_label, gt_crowded, gt_area):
-            gt_anns.append(
-                _create_ann(gt_whole_m, gt_lbl, None,
-                            img_id=img_id, ann_id=len(gt_anns) + 1,
-                            crw=gt_crw, ar=gt_ar))
-            unique_labels[gt_lbl] = True
-        images.append({'id': img_id, 'height': size[0], 'width': size[1]})
+            gt_annos.append(
+                _create_anno(gt_whole_m, gt_lb, None,
+                             img_id=img_id, anno_id=len(gt_annos) + 1,
+                             crw=gt_crw, ar=gt_ar))
+            unique_labels[gt_lb] = True
+        ids.append({'id': img_id, 'height': size[0], 'width': size[1]})
 
     pred_coco.dataset['categories'] = [{'id': i} for i in unique_labels.keys()]
     gt_coco.dataset['categories'] = [{'id': i} for i in unique_labels.keys()]
-    pred_coco.dataset['annotations'] = pred_anns
-    gt_coco.dataset['annotations'] = gt_anns
-    pred_coco.dataset['images'] = images
-    gt_coco.dataset['images'] = images
+    pred_coco.dataset['annotations'] = pred_annos
+    gt_coco.dataset['annotations'] = gt_annos
+    pred_coco.dataset['images'] = ids
+    gt_coco.dataset['images'] = ids
 
     with _redirect_stdout(open(os.devnull, 'w')):
         pred_coco.createIndex()
@@ -97,7 +97,7 @@ def eval_instance_segmentation_coco(sizes, pred_bboxes, pred_masks,
         'prec': ev.eval['precision'],
         'rec': ev.eval['recall'],
         'iou_threshs': p.iouThrs,
-        'area_ranges': p.areaRngLbl,
+        'area_ranges': p.areaRnglb,
         'max_detection_list': p.maxDets}
     all_kwargs = {
         'ap/iou=0.50:0.95/area=all/maxDets=100': {
@@ -146,7 +146,7 @@ def eval_instance_segmentation_coco(sizes, pred_bboxes, pred_masks,
     return results
 
 
-def _create_ann(whole_m, lbl, sc, img_id, ann_id, crw=None, ar=None):
+def _create_anno(whole_m, lb, sc, img_id, anno_id, crw=None, ar=None):
     H, W = whole_m.shape
     if crw is None:
         crw = False
@@ -155,15 +155,15 @@ def _create_ann(whole_m, lbl, sc, img_id, ann_id, crw=None, ar=None):
     # Surprisingly, ground truth ar can be different from area(rle)
     if ar is None:
         ar = mask_tools.area(rle)
-    ann = {
-        'image_id': img_id, 'category_id': lbl,
+    anno = {
+        'image_id': img_id, 'category_id': lb,
         'segmentation': rle,
         'area': ar,
-        'id': ann_id,
+        'id': anno_id,
         'iscrowd': crw}
     if sc is not None:
-        ann.update({'score': sc})
-    return ann
+        anno.update({'score': sc})
+    return anno
 
 
 def _summarize(
@@ -174,27 +174,28 @@ def _summarize(
     a_idx = area_ranges.index(area_range)
     m_idx = max_detection_list.index(max_detection)
     if ap:
-        s = prec.copy()  # (T, R, K, A, M)
+        val_value = prec.copy()  # (T, R, K, A, M)
         if iou_thresh is not None:
-            s = s[iou_thresh == iou_threshs]
-        s = s[:, :, :, a_idx, m_idx]
+            val_value = val_value[iou_thresh == iou_threshs]
+        val_value = val_value[:, :, :, a_idx, m_idx]
     else:
-        s = rec.copy()  # (T, K, A, M)
+        val_value = rec.copy()  # (T, K, A, M)
         if iou_thresh is not None:
-            s = s[iou_thresh == iou_threshs]
-        s = s[:, :, a_idx, m_idx]
+            val_value = val_value[iou_thresh == iou_threshs]
+        val_value = val_value[:, :, a_idx, m_idx]
 
-    s[s == -1] = np.nan
-    s = s.reshape((-1, s.shape[-1]))
-    valid_classes = np.any(np.logical_not(np.isnan(s)), axis=0)
-    class_s = np.nan * np.ones(len(valid_classes), dtype=np.float32)
-    class_s[valid_classes] = np.nanmean(s[:, valid_classes], axis=0)
+    val_value[val_value == -1] = np.nan
+    val_value = val_value.reshape((-1, val_value.shape[-1]))
+    valid_classes = np.any(np.logical_not(np.isnan(val_value)), axis=0)
+    cls_val_value = np.nan * np.ones(len(valid_classes), dtype=np.float32)
+    cls_val_value[valid_classes] = np.nanmean(
+        val_value[:, valid_classes], axis=0)
 
     if not np.any(valid_classes):
-        mean_s = np.nan
+        mean_val_value = np.nan
     else:
-        mean_s = np.nanmean(class_s)
-    return class_s, mean_s
+        mean_val_value = np.nanmean(cls_val_value)
+    return cls_val_value, mean_val_value
 
 
 @contextlib.contextmanager
